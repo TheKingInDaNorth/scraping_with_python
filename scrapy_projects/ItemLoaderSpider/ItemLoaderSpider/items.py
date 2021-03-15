98a23d49d326e79807603564db860338dc7e9efc
TheKingInDaNorth <60161055+TheKingInDaNorth@users.noreply.github.com>
2021-03-15 13:51:16 -0700

    3,15,21

--- scrapy_projects/ItemLoaderSpider/ItemLoaderSpider/items.py
+++ scrapy_projects/ItemLoaderSpider/ItemLoaderSpider/items.py
@@ -5,7 +5,7 @@ def full_links(company_symbol_link):
     url = 'https://finance.yahoo.com/sector/ms_technology/'
     return url + company_symbol_link
 
-class ItemloaderspiderItem(scrapy.item):
+class CompanyDetailsItem(scrapy.Item):
     company_name = scrapy.Field()
     company_symbol_link = scrapy.Field(input_processor = MapCompose(full_links))
     company_price_intraday = scrapy.Field()
